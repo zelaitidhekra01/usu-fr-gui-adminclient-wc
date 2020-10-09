46d1822a599dc4ad83a23a6bbf0f6843572ddd7d
zelaitidhekra01 <zelaiti.dhekra@aspera.com>
2020-10-09 21:37:47 +0200

    delete log

--- src/app/services/password-validation.ts
+++ src/app/services/password-validation.ts
@@ -1,21 +1,16 @@
-import {AbstractControl} from '@angular/forms';
-import {Injectable} from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+import { Injectable } from '@angular/core';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class PasswordValidation {
-
   
-
   static NotMatching(control: AbstractControl) {
-    console.log("=====>PasswordValidation");
     const password = control.get('newPass_word').value;
     const confirmPassword = control.get('confirmPassword').value;
-    console.log("=====>confirmPassword", confirmPassword);
-
     if (password !== confirmPassword) {
-      control.get('confirmPassword').setErrors({NotMatching: true})
+      control.get('confirmPassword').setErrors({ NotMatching: true })
     } else {
       return null
     }
